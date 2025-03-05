6bab516513404c10656b70e06ba3fc37215bb01e
Jagjit <singhjagjitiosuk@gmail.com>
2025-03-05 20:45:11 +0000

    Initial Commit

---
+++ SwiftUIMapApp/Models/Location.swift
@@ -0,0 +1,17 @@
+//
+//  Locations.swift
+//  SwiftUIMapApp
+//
+//  Created by jj on 05/03/2025.
+//
+import Foundation
+import MapKit
+
+struct Location {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
---
+++ SwiftUIMapApp/SwiftUIMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMapAppApp.swift
+//  SwiftUIMapApp
+//
+//  Created by jj on 05/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
